23fdac73413c4249f840f85708cd790222f31dca
Karishnu Poddar <karishnu@gmail.com>
2017-03-11 18:45:25 +0530

    commit

---
+++ app/src/main/java/com/camtech/corneatopo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.camtech.corneatopo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.loadLibrary("opencv_java3");
+    }
+}
